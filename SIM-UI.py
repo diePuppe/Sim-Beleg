77d61b9ae9c5dd26f8fa9144fbbd23d1f8d46432
diePuppe <95628567+diePuppe@users.noreply.github.com>
2023-07-07 09:13:05 +0200

    Update SIM-UI.py

--- SIM-UI.py
+++ SIM-UI.py
@@ -9,30 +9,30 @@ import threading
 # Globale Variable für den aktuellen Simulations-Thread
 current_simulation_thread = None
 
-root = tk.Tk()
-summer = tk.BooleanVar(value=False)  # !!!!!STELLSCHRAUBE!!!!!: Anpassbarer Parameter für Sommer/Winter
+root = tk.Tk()  # Initialisieren des root-GUI-Fensters
+summer = tk.BooleanVar(value=False)
 semester_ferien = tk.BooleanVar(value=False)
-selected_beer = tk.StringVar(value="Becks")  # !!!!!STELLSCHRAUBE!!!!!: Anpassbarer Parameter für Biersorte
+selected_beer = tk.StringVar(value="Becks")
 
 
 class Customer:
     def __init__(self, env, store, customer_type):
         self.env = env
         self.store = store
-        self.customer_type = customer_type
+        self.customer_type = customer_type  # student oder normal
 
     def buy_beer(self):
         if self.customer_type == 'Student':
             if semester_ferien.get():
-                if random.random() <= 0.05:  # Nur 5% der Studenten kaufen während der Semesterferien ein
-                    amount = random.randint(2, 20)
+                if random.random() <= 0.05:  # Stellschraube: Wie viele Studenten kaufen während der Semesterferien ein
+                    amount = random.randint(2, 20)  # Studenten kaufen während der Semesterferien mehr
                 else:
-                    return 0  # Rückgabe von 0 für Studenten, die nicht während der Semesterferien einkaufen
+                    return 0
             else:
-                amount = random.randint(2, 10)  # Students buy 2 times more beer
+                amount = random.randint(2, 5)  # Students kaufen 2-5 Bier während der Vorlesung  # Stellschraube
         else:
-            amount = random.randint(1, 3)  # Regular customers buy regular amount of beer
-        
+            amount = random.randint(1, 3)  # Normale Menschen kaufen 1-3 Bier
+
         beer_multiplier = 1.0
         if selected_beer.get() == "Landskron":
             beer_multiplier = 2.5  # Verkauf erhöht sich um 150%
@@ -44,27 +44,27 @@ class Customer:
             beer_multiplier = 2.0  # Verkauf steigt um 100%
 
         if summer.get():
-            beer_multiplier *= 2  # Double the amount of beer sold in summer
+            beer_multiplier *= 2  # Im Sommer verdoppelt sich der Verkauf
 
         amount = int(amount * beer_multiplier)
-        
+
         yield from self.store.buy_beer(amount)
-        return amount  # This line is added to return the 'amount'
+        return amount
 
 
 class BeerStore:
     def __init__(self, env, fig, canvas):
         self.env = env
-        self.store = simpy.Container(env, init=10000, capacity=10000)
+        self.store = simpy.Container(env, init=10000, capacity=10000)  # ToDo: Nachfüllen des Bierbestandes
         self.total_beer_sold = 0
         self.student_beer_sales = [0] * 30
         self.daily_beer_sales = [0] * 30
         self.daily_customers = [0] * 30
         self.fig = fig
         self.canvas = canvas
-        self.ax = self.fig.add_subplot(131)  # Beer sales axis
-        self.ax2 = self.fig.add_subplot(132)  # Customers count axis
-        self.ax3 = self.fig.add_subplot(133)  # Student beer sales axis
+        self.ax = self.fig.add_subplot(131)  # Bierverkauf Diagramm
+        self.ax2 = self.fig.add_subplot(132)  # Kunden Diagramm
+        self.ax3 = self.fig.add_subplot(133)  # Verkauf an Studenten Diagramm
 
     def buy_beer(self, amount):
         self.total_beer_sold += amount
@@ -98,15 +98,15 @@ class BeerStore:
         return Customer(self.env, self, customer_type)
 
     def run_simulation(self):
-        for _ in range(30):
-            num_customers = random.randint(10, 500)  # Random number of customers
+        for _ in range(31):
+            num_customers = random.randint(10, 500)  # Anzahl der Kunden
             for _ in range(num_customers):
-                customer_type = random.choice(['Regular', 'Student'])
+                customer_type = random.choice(['Regular', 'Student'])  # Hier könnte man noch angeben, in welchem Verhältnis Studenten und Kunden kommen
                 customer = self.add_customer(customer_type)
-                amount = yield from customer.buy_beer()  # This is changed
+                amount = yield from customer.buy_beer()
                 if customer_type == 'Student' and amount > 0:  # Nur wenn der Student tatsächlich Bier kauft
                     self.student_buy_beer(amount)
-            yield self.env.timeout(1)  # A day passes
+            yield self.env.timeout(1)  # Ein Tag vergeht
 
 
 def simulation_thread(fig, canvas):
@@ -126,7 +126,7 @@ def main(fig, canvas):
     current_simulation_thread.start()
 
 
-fig = Figure(figsize=(15, 5), dpi=100)  # Changed figure size to better accommodate three subplots in a row
+fig = Figure(figsize=(15, 5), dpi=100)  # Größe ändern, um Diagramme besser zu "fitten"
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack()
 
@@ -137,9 +137,9 @@ start_button.pack()
 season_label = tk.Label(root, text="Saison:")
 season_label.pack()
 season_combobox = ttk.Combobox(root, values=["Sommer", "Winter"], state="readonly")
-season_combobox.current(0)  # Set default value to "Sommer"
+season_combobox.current(0)  # Standardwert ist "Sommer"
 season_combobox.pack()
-summer.set(True)  # Set summer as default season
+summer.set(True)
 
 def season_changed(event):
     if season_combobox.get() == "Sommer":
@@ -156,8 +156,8 @@ semester_ferien_checkbutton.pack()
 # Dropdown-Liste für Biersorten
 beer_label = tk.Label(root, text="Biersorte:")
 beer_label.pack()
-beer_combobox = ttk.Combobox(root, values=["Becks", "Landskron", "Hasseröder", "Feldschlößchen", "Helles"], state="readonly", textvariable=selected_beer)
-beer_combobox.current(0)  # Set default value to "Becks"
+beer_combobox = ttk.Combobox(root, values=["Becks", "Landskron", "Hasseröder", "FeldSchlößchen", "Helles"], state="readonly", textvariable=selected_beer)
+beer_combobox.current(0)  # Standardwert ist "Becks"
 beer_combobox.pack()
 
 root.mainloop()
